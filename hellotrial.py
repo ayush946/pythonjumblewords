db43e640f98430c772a3de3cab1dc248bbe2710a
Ananya <65886348+Ananyaiitbhilai@users.noreply.github.com>
2021-02-15 12:58:09 +0530

    Update hellotrial.py

--- hellotrial.py
+++ hellotrial.py
@@ -1,2 +1,10 @@
 print("hello ananya")
 print("Test commit 2")
+n=str(input("enter your name"))
+if (n=="ananya"):
+     print("u can't say hello to urself")
+else:
+     print("hello",n)
+  
+  
+  
